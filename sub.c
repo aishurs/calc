6238a6f0e1208c00f1994e30d5586666ae581e9e
tejalbayaskar <tejalbayaskar147@gmail.com>
2016-07-31 05:44:26 +0530

    subtraction

---
+++ sub.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+	int res,a,b;
+	res=sub(a,b);
+	printf("Result of subtraction is=%d",res);
+}
+
+int sub(int no1,int no2)
+{
+	int s;
+	s=no1-no2;
+	return s;
+}
+	
